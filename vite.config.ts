a5d41b87bd4d9fdb3cc1f49fd8a392fa8e485a4b
sammig6i <swghazzawi@gmail.com>
2025-10-17 14:20:45 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
-import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  ssr: {
+    noExternal: ["@tanstack/router-core", "@tanstack/*"],
+  },
   server: {
     port: 3000,
   },
